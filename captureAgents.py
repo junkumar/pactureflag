4f0750d0a94dea15bd99e4ba28e5e06d2cdbbb63
Nandu Jayakumar <nandu@cs.stanford.edu>
2011-03-08 11:39:52 -0800

    we want maze distances

--- captureAgents.py
+++ captureAgents.py
@@ -94,8 +94,7 @@ class CaptureAgent(Agent):
     self.red = gameState.isOnRedTeam(self.index)
     self.distancer = distanceCalculator.Distancer(gameState.data.layout)
     
-    # comment this out to forgo maze distance computation and use manhattan distances
-    # self.distancer.getMazeDistances()
+    self.distancer.getMazeDistances()
     
     import __main__
     if '_display' in dir(__main__):
